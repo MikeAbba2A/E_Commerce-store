05e3f99dc7b6ee113240cd9de1f79ba56ae9bc44
MikeAbba2A <119350000+MikeAbba2A@users.noreply.github.com>
2024-05-27 15:01:18 +0200

    ajout description

--- components/info.tsx
+++ components/info.tsx
@@ -18,11 +18,10 @@ const Info: React.FC<InfoProps> = ({
     const onAddToCart = () => {
         cart.addItem(data);
     }
+
     return (
         <div>
-            <h1 
-                className="text-3xl font-bold text-gray-900">{data.name}
-            </h1>
+            <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
             <div className="mt-3 flex items-end justify-between">
                 <p className="text-2xl text-gray-900">
                     <Currency value={data?.price} />
@@ -40,9 +39,15 @@ const Info: React.FC<InfoProps> = ({
                     <h3 className="font-semibold text-black">Couleur:</h3>
                     <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
                 </div>
+                <div className="flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Description:</h3>
+                    <div>
+                        {data?.description}
+                    </div>
+                </div>
             </div>
-            <div className="mt-10 felx items-center gap-x-3">
-                <Button onClick={onAddToCart} className="flex itemps-center gap-x-2">
+            <div className="mt-10 flex items-center gap-x-3">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                     Ajouter au panier
                     <ShoppingCart />
                 </Button>
