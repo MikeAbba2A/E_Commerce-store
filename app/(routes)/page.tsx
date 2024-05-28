448156122f22aa4cf9fb2ffcb53299cbab6c3f96
MikeAbba2A <119350000+MikeAbba2A@users.noreply.github.com>
2024-05-28 21:28:18 +0200

    ajout billboard basket sur la page basket 3

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,7 @@
-import getBillboard from "@/actions/get-billboard";
+// import getBillboard from "@/actions/get-billboard";
+// import Billboard from "@/components/billboard";
+
 import getProducts from "@/actions/get-products";
-import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
@@ -8,11 +9,11 @@ export const revalidate = 0;
 
 const HomePage = async () => {
     const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard("c7330112-a334-4671-bfbc-84d36060b664");
+    // const billboard = await getBillboard("c7330112-a334-4671-bfbc-84d36060b664");
     return (
         <Container>
             <div className="space-y-10 pb-10">
-                <Billboard data={billboard} />
+                {/* <Billboard data={billboard} /> */}
             
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
                     <ProductList title="Produits populaires" items={products} />
