f11eff51d4a3331663254514f885809a60cf51ae
MikeAbba2A <119350000+MikeAbba2A@users.noreply.github.com>
2024-08-01 15:11:06 +0200

    SLIDER 4

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -10,19 +10,24 @@ const ProductSlider = dynamic(() => import('@/components/ProductSlider'), { ssr:
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const featuredProducts = await getProducts({ isFeatured: true });
-  const allProducts = await getProducts({});
-
-  return (
-    <Container>
-      <div className="space-y-10 pb-10">
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Produits populaires" items={featuredProducts} />
+  try {
+    const featuredProducts = await getProducts({ isFeatured: true });
+    const allProducts = await getProducts({});
+  
+    return (
+      <Container>
+        <div className="space-y-10 pb-10">
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title="Produits populaires" items={featuredProducts} />
+          </div>
+          <ProductSlider products={allProducts} />
         </div>
-        <ProductSlider products={allProducts} />
-      </div>
-    </Container>
-  );
+      </Container>
+    );
+  } catch (error) {
+    console.error('Erreur lors du rendu de la page d\'accueil:', error);
+    return <div>Erreur lors du chargement des produits.</div>;
+  }
 };
-// sdfg
+
 export default HomePage;
